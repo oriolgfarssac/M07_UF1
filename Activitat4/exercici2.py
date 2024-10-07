d78abad2080924e5a0497a57f083ebb72d9ea092
danisancheziticbcn <2023_daniel.sanchez@iticbcn.cat>
2024-10-07 18:59:34 +0200

    act4exercici2acabat

---
+++ Activitat4/exercici2.py
@@ -0,0 +1,14 @@
+import numpy as np
+def creacioarray1():
+    array1 = [88, 23, 39, 41]
+    array1
+    return array1
+def creacioarray2():
+   array2 = [[76.4, 21.7, 38.4], [41.2, 52.8, 68.9]]
+   return array2
+def creacioarray3():
+   array3 = [[12], [4], [9], [8]]
+   return array3
+print(creacioarray1())
+print(creacioarray2())
+print(creacioarray3())
