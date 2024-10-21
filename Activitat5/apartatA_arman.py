33e3de27a5d023bb31635e4fc099cdba1d50e625
Arman Mohammed Akther <2023_arman.mohammed@iticbcn.cat>
2024-10-21 17:02:08 +0200

    Readme afegit, main y apartat A corregida

--- Activitat5/apartatA_arman.py
+++ Activitat5/apartatA_arman.py
@@ -45,3 +45,51 @@ def funcio1():
     # Dataframe on es guarda el resultat
     resultats_df = pd.DataFrame(resultats)
     return resultats_df
+
+def funcio2():
+
+    df = pd.read_csv('df_covid19_countries.csv', usecols=['location', 'date', 'total_deaths'])
+
+    paisos = ['Afghanistan', 'Armenia', 'China', 'Georgia', 'Liberia', 'Guatemala', 'Tanzania', 'Turkmenistan', 'Uruguay', 'Zimbabwe']
+
+    filaPais = []
+
+    for i in range(len(df)):
+        if df.loc[i, 'location'] in paisos:
+            filaPais.append(df.loc[i])
+
+    # DataFrame on es guarda nomes les files del paisos seleccionats
+    dfPaisos = pd.DataFrame(filaPais)
+
+    meses = ['2021-01', '2021-02', '2021-03', '2021-04']
+
+    filas = []
+
+    for i in range(len(dfPaisos)):
+        mes_actual = dfPaisos.iloc[i]['date'][:7]
+        if mes_actual in meses:
+            filas.append(dfPaisos.iloc[i])
+
+    # DataFrame on es guarda nomes les files amb els paisos i els mesos seleccionats
+    dfPaisosMesos = pd.DataFrame(filas)
+
+    resultats = []
+
+    for i in range(len(paisos)):
+        for mes in meses:
+
+            filtrePais = dfPaisosMesos['location'] == paisos[i]
+
+            filtreMes = dfPaisosMesos['date'].str[:7] == mes
+
+            morts = dfPaisosMesos[filtrePais & filtreMes]['total_deaths']
+
+            suma = morts.sum()
+
+            resultats.append({'location': paisos[i], 'month': mes, 'total_deaths': suma})
+    # Dataframe on es guarda el resultat
+    resultats_df = pd.DataFrame(resultats)
+    return resultats_df
+
+
+
